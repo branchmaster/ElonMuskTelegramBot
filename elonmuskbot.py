1b8aa86261796c5f0313bbd3cbb54b656bcec117
IncrociatoreInterstellare\Lando Calrissian <sbdsurface@gmail.com>
2021-02-27 21:37:51 +0100

    job.run_repeating

--- elonmuskbot.py
+++ elonmuskbot.py
@@ -1,20 +1,15 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import twint
-#import nest_asyncio
-import random
-import re
-import requests
-import shutil
-import time
-import datetime
+
 #quotefile = open('quotes.txt','r')
 #linelist = quotefile.read().split('\n') 
 #quotefile.close()
 
 TOKEN=""
-
-def mining(update,context): 
-   oldtweetid = 1
+CHATID=123
+oldtweetid = 123
+def callbacktweet(context: CallbackContext): 
+   global oldtweetid
    tweets = [] 
    #Twint configuration
    t = twint.Config() 
@@ -22,33 +17,28 @@ def mining(update,context):
    t.Limit = 1
    t.Filter_retweets = False
    t.Store_object = True
-   
-  
-   while True:
-      time.sleep(10)
-      tweets = []
-      t.Store_object_tweets_list = tweets
-      twint.run.Search(t)
-      newtweet = tweets[0]
+   tweets = []
+   t.Store_object_tweets_list = tweets
+   twint.run.Search(t)
+   newtweet = tweets[0]
+   #check if the latest tweet it hasn't been reported yet and exclude the tweet replies @
+   if(newtweet.id != oldtweetid and newtweet.tweet[0] != '@'):
+      if (len(newtweet.photos) > 0): 
+         #erase the link that store the photos from the text
+         newtweet.tweet = re.sub(r'(https:\/\/t\.co\/..........)','',newtweet.tweet) 
+         if(len(newtweet.tweet) > 1): context.bot.send_message(chat_id=CHATID, text=newtweet.tweet)
+         for imageurl in newtweet.photos:
+            context.bot.send_photo(CHATID,imageurl)
+      elif newtweet.video == 1:
+         context.bot.send_message(chat_id=CHATID, text=newtweet.link)
+      else:
+         context.bot.send_message(chat_id=CHATID, text=newtweet.tweet) 
+   oldtweetid = newtweet.id
 
-      #check if the latest tweet it hasn't been reported yet and exclude the tweet replies @
-      if(newtweet.id != oldtweetid and newtweet.tweet[0] != '@'):
-         if (len(newtweet.photos) > 1): 
-            #erase the link that store the photos from the text
-            newtweet.tweet = re.sub(r'(https:\/\/t\.co\/..........)','',newtweet.tweet) 
-            if(len(newtweet.tweet) > 1): context.bot.send_message(chat_id=update.effective_chat.id, text=newtweet.tweet)
-            for imageurl in newtweet.photos:
-               context.bot.send_photo(update.effective_chat.id,imageurl)
-         elif newtweet.video == 1:
-            context.bot.send_message(chat_id=update.effective_chat.id, text=newtweet.link)
-         else:
-            context.bot.send_message(chat_id=update.effective_chat.id, text=newtweet.tweet) 
-      oldtweetid = newtweet.id
-   
 def main():
    upd= Updater(TOKEN, use_context=True)
-   disp=upd.dispatcher
-   disp.add_handler(CommandHandler("mining", mining))
+   job = upd.job_queue
+   job.run_repeating(callbacktweet,interval=60,first=1)
    upd.start_polling()
    upd.idle()
 
